641369b0f2ae074bce80c96c8756a761f640d897
Sebastiaan Crisan <sebastiaancrisan@gmail.com>
2023-10-06 14:24:12 +0200

    WIP: update styling for mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,56 +37,60 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl font-bold mb-4">League of Legends Nemesis</h1>
-      <form onSubmit={handleSubmit} className="w-full max-w-md">
-        <label className="block mb-4">
-          Summoner Name:
-          <input
-            type="text"
-            value={summonerName}
-            onChange={(e) => setSummonerName(e.target.value)}
-            required
-            className="mt-2 p-3 w-full border rounded-md"
-          />
-        </label>
-        <button
-          type="submit"
-          disabled={loading}
-          className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white p-2 rounded-md w-full transition duration-150"
-        >
-          {loading ? "Loading..." : "Fetch Data"}
-        </button>
-      </form>
-      {loading && <div className="loader mt-4"></div>} {/* Loading spinner */}
-      {data.length > 0 && (
-        <div className="result mt-8 bg-white shadow-lg rounded-lg p-4 w-full max-w-md">
-          <h2 className="text-xl font-bold mb-4">Results:</h2>
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead>
-              <tr>
-                <th className="px-4 py-2">Index</th>
-                <th className="px-4 py-2">Champion</th>
-                <th className="px-4 py-2">Games Played Against</th>
-                <th className="px-4 py-2">Loss Percentage</th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {data.map((item, idx) => (
-                <tr
-                  key={item.index}
-                  className={idx % 2 === 0 ? "bg-gray-50" : ""}
-                >
-                  <td className="px-4 py-2">{item.index}</td>
-                  <td className="px-4 py-2">{item.champion}</td>
-                  <td className="px-4 py-2">{item.count}</td>
-                  <td className="px-4 py-2">{item.loss_percentage}%</td>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 sm:px-8">
+      <div className="w-full max-w-md mx-auto overflow-hidden">
+        <h1 className="text-center text-2xl sm:text-3xl font-bold mb-4">
+          League of Legends Nemesis
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <label className="block mb-4">
+            Summoner Name:
+            <input
+              type="text"
+              value={summonerName}
+              onChange={(e) => setSummonerName(e.target.value)}
+              required
+              className="mt-2 p-3 w-full border rounded-md text-lg sm:text-xl"
+            />
+          </label>
+          <button
+            type="submit"
+            disabled={loading}
+            className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white p-3 rounded-md w-full transition duration-150 text-lg sm:text-xl"
+          >
+            {loading ? "Loading..." : "Fetch Data"}
+          </button>
+        </form>
+        {loading && <div className="loader mt-4"></div>} {/* Loading spinner */}
+        {data.length > 0 && (
+          <div className="result mt-8 bg-white shadow-lg rounded-lg p-4 w-full max-w-md">
+            <h2 className="text-xl font-bold mb-4">Results:</h2>
+            <table className="min-w-full divide-y divide-gray-200 table-auto">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2">Index</th>
+                  <th className="px-4 py-2">Champion</th>
+                  <th className="px-4 py-2">Games Played Against</th>
+                  <th className="px-4 py-2">Loss Percentage</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {data.map((item, idx) => (
+                  <tr
+                    key={item.index}
+                    className={idx % 2 === 0 ? "bg-gray-50" : ""}
+                  >
+                    <td className="px-4 py-2">{item.index}</td>
+                    <td className="px-4 py-2">{item.champion}</td>
+                    <td className="px-4 py-2">{item.count}</td>
+                    <td className="px-4 py-2">{item.loss_percentage}%</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
